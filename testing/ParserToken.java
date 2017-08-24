b4be795aead2236a77d1c836cb9e0624b272fb75
Anastasiya Shadrina <a.shadrina5@mail.ru>
2017-08-24 16:21:27 +0700

    Add files via upload

---
+++ testing/ParserToken.java
@@ -0,0 +1,27 @@
+package testing;
+
+public class ParserToken {
+    private String tokenType;
+    private String tokenText;
+
+    public ParserToken() {
+        this.tokenType = "";
+        this.tokenText = "";
+    }
+
+    public String getTokenType() {
+        return tokenType;
+    }
+
+    public String getTokenText() {
+        return tokenText;
+    }
+
+    public void setTokenType(String tokenType_) {
+        this.tokenType = tokenType_;
+    }
+
+    public void setTokenText(String tokenText_) {
+        this.tokenText = tokenText_;
+    }
+}
